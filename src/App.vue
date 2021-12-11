a07e3646baf2ecadf08b7496230058c4d17f0460
Kaliszby <sumyung17qwq@gmail.com>
2021-12-11 13:52:19 +0700

    Ref Side Effect

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,41 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <div>
+    <span>count: {{ count }}</span
+    ><br />
+    <span>count1: {{ count1 }}</span
+    ><br />
+    <button @click="onPlus">plus</button>
+    <button @click="onPlus1">plus1</button>
+  </div>
+  <router-view />
 </template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    let count = 0; // No side effect ตัวแปรเปลี่ยนค่าแต่ไม่ส่งผลในหน้า UI
+    const count1 = ref<number>(0); // side effect ตัวแปรเปลี่ยนค่าและส่งผลในหน้า UI
+    const onPlus = () => {
+      count = count + 1;
+      console.log(count);
+    };
+
+    const onPlus1 = () => {
+      count1.value = count1.value + 1;
+      console.log(count1);
+    };
+    return {
+      count,
+      count1,
+      onPlus,
+      onPlus1,
+    };
+  },
+});
+</script>
 
 <style>
 #app {
