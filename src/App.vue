e6d97bb80e4a1b14dd58e7f96069bb050063912c
Kaliszby <sumyung17qwq@gmail.com>
2021-12-11 14:37:23 +0700

    Child-Component

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,22 @@
     <router-link to="/about">About</router-link>
   </div>
   <div>
-    <span>{{ account1.data }}</span
-    ><br />
-    <span>{{ count }}</span
-    ><br />
-    <button @click="onClickClear">Clear</button>
+    <hr />
+    <LoginForm />
+    <hr />
   </div>
   <router-view />
 </template>
 <script lang="ts">
-import { defineComponent, ref, onMounted, reactive } from "vue";
-
-const defaultAcc = { username: "", password: "" };
+import { defineComponent } from "vue";
+import LoginForm from "@/components/LoginForm.vue";
 
 export default defineComponent({
+  components: {
+    LoginForm,
+  },
   setup() {
-    const count = ref<number>(0);
-    const account = reactive({ username: "admin", password: "123456" }); // input object only
-    const account1 = reactive({
-      data: { username: "admin", password: "123456" },
-    });
-    const onClickClear = () => {
-      // account.username = "";
-      // account.password = "";
-      account1.data = defaultAcc;
-    };
-
-    const onPlus = () => {
-      count.value = count.value + 1;
-    };
-
-    onMounted(() => {
-      setInterval(onPlus, 1000);
-    });
-
-    return {
-      account,
-      account1,
-      onClickClear,
-      onMounted,
-      count,
-    };
+    return {};
   },
 });
 </script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Login Form</h1>
+    <br />
+    <span>user</span><br />
+    <span>password</span>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
