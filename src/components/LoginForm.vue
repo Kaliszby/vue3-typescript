db3f4de5aeed92afab8405107233b5f98ccced11
Kaliszby <sumyung17qwq@gmail.com>
2021-12-11 15:05:48 +0700

    V-Model

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,18 +3,40 @@
     <h1>Login</h1>
     <br />
     <span>{{ user }}</span>
+    <form>
+      <label for="username">Username</label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        v-model="from_data.account.username"
+      />
+      <br />
+      <label for="password">Password</label>
+      <input
+        type="text"
+        name="password"
+        id="password"
+        v-model="from_data.account.password"
+      />
+      <br />
+      {{ from_data.account }}
+      <button type="button">Submit</button>
+      <button type="button" @click="onClear">Clear</button>
+    </form>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
   props: ["user"],
-  setup(props) {
-    onMounted(() => {
-      console.log(props.user);
-    });
-    return { onMounted };
+  setup() {
+    const from_data = reactive({ account: { username: "", password: "" } });
+    const onClear = () => {
+      from_data.account = { username: "", password: "" };
+    };
+    return { from_data, onClear };
   },
 });
 </script>
